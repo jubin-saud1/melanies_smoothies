6b687dbec77e420b491e6853b1258a52cf93334d
jubin-saud1 <jubin.saud@triedatum.com>
2025-06-11 00:49:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruits in ingredients_list:
         ingredients_string += fruits + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
         
     #st.write(ingredients_string)
 
@@ -54,7 +57,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+
 
