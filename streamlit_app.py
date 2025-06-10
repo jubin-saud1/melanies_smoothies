801fd9155f0f64d2e25a3ad25bce314e35a6a08a
jubin-saud1 <jubin.saud@triedatum.com>
2025-06-11 01:28:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ if ingredients_list:
     for fruits in ingredients_list:
         ingredients_string += fruits + ' '
       
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits,' is ', search_on, '.')
 
       
         st.subheader(fruits + ' nutrition information')
